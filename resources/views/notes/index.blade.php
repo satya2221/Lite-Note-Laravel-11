36f5e3f3a47545394ae6e2c7ec99f9a0da92e038
satya2221 <satya.adipratama@taspenlife.com>
2024-12-30 16:35:22 +0700

    feat: pagination of data and string trim

--- resources/views/notes/index.blade.php
+++ resources/views/notes/index.blade.php
@@ -10,11 +10,13 @@
             @forelse ($notes as $note)
             <div class="bg-white p-6 overflow-hidden shadow-sm sm:rounded-lg">
                 <h2 class="font-bold text-2xl text-indigo-600">{{ $note->title }}</h2>
-                <p class="mt-2 ">{{ $note-> text }}</p>
+                <p class="mt-2 ">{{ Str::limit($note-> text, 200, '...')  }}</p>
                 <span class="block mt-4 text-sm opacity-70">{{ $note->updated_at->diffForHumans() }}</span>
             </div>
             @empty
             <h1>You have no notes yet</h1>
             @endforelse
+
+            {{ $notes->links() }}
     </div>
 </x-app-layout>
